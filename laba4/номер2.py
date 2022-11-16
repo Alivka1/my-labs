5c6d5b54d9e3bd973b654206d4643f6d6fc63432
Alivka1 <113111963+Alivka1@users.noreply.github.com>
2022-11-16 23:48:04 +0300

    Delete laba4 directory

--- laba4/номер2.py
+++
@@ -1,8 +0,0 @@
-any_list = [1,2,3,4,3,2,1,1,2,3,4,6,789,0,99999]
-result = []
-def more(list):
-    for i in range(len(list)):
-        if list[i] > list[i - 1]:
-            result.append(list[i])
-    print(result)
-more(any_list)
